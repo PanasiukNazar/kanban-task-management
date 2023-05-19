2c38313438b984c6edea713180548869ba661c1a
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2023-05-19 09:37:49 +0300

    create NoColumnsBoard component

---
+++ client/src/components/NoColumnsBoard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="no-column-board">
+        <div class="add-column">
+            <p class="title">
+                This board is empty. Create a new column to get started.
+            </p>
+            <div class="button-wrapper">
+                <button>+ Add New Column</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+@import '../styles/main.scss';
+
+.no-column-board {
+    background: $minor_background;
+    height: 100vh;
+
+    .add-column {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        .title {
+            margin: 0;
+            font-style: normal;
+            color: $minor_color;
+            font-weight: $bold_text;
+            font-family: $regular_family;
+            font-size: $medium_font_size;
+            line-height: $medium_line_height;
+            margin-bottom: $medium_margin;
+        }
+
+        .button-wrapper {
+            @include common-button;
+        }
+    }
+}
+</style>
