761066e44a811060f42defd8f8f77a5017def0cc
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2023-05-19 09:41:17 +0300

    change the structure of the state

--- client/src/store/state.js
+++ client/src/store/state.js
@@ -1,23 +1,292 @@
 export default {
     isDarkTheme: true,
     isSidebarHidden: false,
-    boardItems: [
+    boards: [
         {
             title: 'Platform launch',
             id: 1,
-            isSelected: false,
         },
         {
             title: 'Marketing plan',
             id: 2,
-            isSelected: false,
-            tasks: [],
         },
         {
             title: 'Roadmap',
             id: 3,
-            isSelected: false,
-            tasks: [],
         },
     ],
+
+    localBoardsMap: {
+        1: {
+            title: 'Platform launch',
+            id: 1,
+            columns: [
+                {
+                    title: 'todo',
+                    id: 1,
+                    color: '#ff0000',
+
+                    tasks: [
+                        {
+                            title: 'UI design',
+                            id: 1,
+
+                            subtasks: [
+                                {
+                                    title: 'first subtasks',
+                                    checked: true,
+                                    id: 1,
+                                },
+                                {
+                                    title: 'second subtasks',
+                                    checked: true,
+                                    id: 2,
+                                },
+                                {
+                                    title: 'third subtasks',
+                                    checked: true,
+                                    id: 3,
+                                },
+                            ],
+                        },
+                        {
+                            title: 'UI design',
+                            id: 1,
+
+                            subtasks: [
+                                {
+                                    title: 'first subtasks',
+                                    checked: true,
+                                    id: 1,
+                                },
+                                {
+                                    title: 'second subtasks',
+                                    checked: true,
+                                    id: 2,
+                                },
+                                {
+                                    title: 'third subtasks',
+                                    checked: true,
+                                    id: 3,
+                                },
+                            ],
+                        },
+                        {
+                            title: 'UI design',
+                            id: 1,
+
+                            subtasks: [
+                                {
+                                    title: 'first subtasks',
+                                    checked: true,
+                                    id: 1,
+                                },
+                                {
+                                    title: 'second subtasks',
+                                    checked: true,
+                                    id: 2,
+                                },
+                                {
+                                    title: 'third subtasks',
+                                    checked: true,
+                                    id: 3,
+                                },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    title: 'todo',
+                    id: 1,
+                    color: '#ff0000',
+
+                    tasks: [
+                        {
+                            title: 'UI design',
+                            id: 1,
+
+                            subtasks: [
+                                {
+                                    title: 'first subtasks',
+                                    checked: true,
+                                    id: 1,
+                                },
+                                {
+                                    title: 'second subtasks',
+                                    checked: true,
+                                    id: 2,
+                                },
+                                {
+                                    title: 'third subtasks',
+                                    checked: true,
+                                    id: 3,
+                                },
+                            ],
+                        },
+                        {
+                            title: 'UI design',
+                            id: 1,
+
+                            subtasks: [
+                                {
+                                    title: 'first subtasks',
+                                    checked: true,
+                                    id: 1,
+                                },
+                                {
+                                    title: 'second subtasks',
+                                    checked: true,
+                                    id: 2,
+                                },
+                                {
+                                    title: 'third subtasks',
+                                    checked: true,
+                                    id: 3,
+                                },
+                            ],
+                        },
+                        {
+                            title: 'UI design',
+                            id: 1,
+
+                            subtasks: [
+                                {
+                                    title: 'first subtasks',
+                                    checked: true,
+                                    id: 1,
+                                },
+                                {
+                                    title: 'second subtasks',
+                                    checked: true,
+                                    id: 2,
+                                },
+                                {
+                                    title: 'third subtasks',
+                                    checked: true,
+                                    id: 3,
+                                },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    title: 'todo',
+                    id: 1,
+                    color: '#ff0000',
+
+                    tasks: [
+                        {
+                            title: 'UI design',
+                            id: 1,
+
+                            subtasks: [
+                                {
+                                    title: 'first subtasks',
+                                    checked: true,
+                                    id: 1,
+                                },
+                                {
+                                    title: 'second subtasks',
+                                    checked: true,
+                                    id: 2,
+                                },
+                                {
+                                    title: 'third subtasks',
+                                    checked: true,
+                                    id: 3,
+                                },
+                            ],
+                        },
+                        {
+                            title: 'UI design',
+                            id: 1,
+
+                            subtasks: [
+                                {
+                                    title: 'first subtasks',
+                                    checked: true,
+                                    id: 1,
+                                },
+                                {
+                                    title: 'second subtasks',
+                                    checked: true,
+                                    id: 2,
+                                },
+                                {
+                                    title: 'third subtasks',
+                                    checked: true,
+                                    id: 3,
+                                },
+                            ],
+                        },
+                        {
+                            title: 'UI design',
+                            id: 1,
+
+                            subtasks: [
+                                {
+                                    title: 'first subtasks',
+                                    checked: true,
+                                    id: 1,
+                                },
+                                {
+                                    title: 'second subtasks',
+                                    checked: true,
+                                    id: 2,
+                                },
+                                {
+                                    title: 'third subtasks',
+                                    checked: true,
+                                    id: 3,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+        2: {
+            title: 'Marketing plan',
+            id: 2,
+            columns: [
+                {
+                    title: 'doing',
+                    id: 1,
+                    color: '#ff0000',
+
+                    tasks: [
+                        {
+                            title: 'UI design',
+                            id: 1,
+
+                            subtasks: [
+                                {
+                                    title: 'first subtasks',
+                                    checked: true,
+                                    id: 1,
+                                },
+                                {
+                                    title: 'second subtasks',
+                                    checked: true,
+                                    id: 2,
+                                },
+                                {
+                                    title: 'third subtasks',
+                                    checked: true,
+                                    id: 3,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+        3: {
+            title: 'Roadmap',
+            id: 3,
+            columns: [],
+        },
+    },
+
+    activeBoard: null,
 };
