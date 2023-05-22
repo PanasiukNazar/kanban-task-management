1263eec012e9b36d6794efea7147bc8517d73f36
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2023-05-22 18:33:27 +0300

    replace columns into separate component "Column.vue"

--- client/src/components/Board.vue
+++ client/src/components/Board.vue
@@ -1,32 +1,12 @@
 <template>
     <div class="board">
-        <div class="no-columns-board">
-            <router-view></router-view>
-        </div>
+        <NoColumnsBoard v-if="!boardLength" />
 
         <div
             class="columns-board"
             :class="!isSidebarHidden ? 'columns-board--reduced' : ''"
         >
-            <div class="column" v-for="column in activeBoard?.columns">
-                <div class="column__header">
-                    <div
-                        class="color"
-                        :style="{ background: column.color }"
-                    ></div>
-                    <h4 class="title">
-                        {{ column.title }} ({{ column.tasks?.length }})
-                    </h4>
-                </div>
-
-                <div class="task" v-for="task in column.tasks">
-                    <h4 class="task__title">{{ task.title }}</h4>
-
-                    <div class="subtasks">
-                        <p>0 of 3 subtasks</p>
-                    </div>
-                </div>
-            </div>
+            <Column v-for="column in activeBoard?.columns" :column="column" />
 
             <div class="add-column">
                 <h3 class="title">+ New Column</h3>
@@ -37,15 +17,17 @@
 
 <script setup>
 import { useStore } from 'vuex';
-import { onBeforeRouteUpdate, useRoute, useRouter } from 'vue-router';
+import { onBeforeRouteUpdate, useRoute } from 'vue-router';
 import { onMounted, computed } from 'vue';
+import NoColumnsBoard from './NoColumnsBoard.vue';
+import Column from './Column.vue';
 
 const route = useRoute();
-const router = useRouter();
 const store = useStore();
 
 const activeBoard = computed(() => store.state.activeBoard);
 const isSidebarHidden = computed(() => store.state.isSidebarHidden);
+const boardLength = computed(() => store.getters.getBoardColumnLength);
 
 function setActiveBoard(id) {
     store.commit('setActiveBoard', id);
@@ -56,89 +38,21 @@ onMounted(() => setActiveBoard(route.params.id));
 onBeforeRouteUpdate((to) => {
     setActiveBoard(to.params.id);
 });
-
-// const boardColumnLength = store.getters.getBoardColumnLength;
-// !boardColumnLength ? router.push(to.path + '/no-columns') : '';
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '../styles/main.scss';
 
 .board {
     height: 100vh;
-    position: relative;
     background: $minor_background;
-    padding-top: $universal_backdown;
 
     .columns-board {
         display: flex;
         position: absolute;
         left: 0;
         transition: all 0.5s ease;
-
-        .column {
-            width: 280px;
-            margin-left: $universal_backdown;
-            margin-right: $universal_backdown;
-
-            .column__header {
-                display: flex;
-                align-items: center;
-                justify-content: start;
-                margin-bottom: $universal_backdown;
-
-                .color {
-                    width: 15px;
-                    height: 15px;
-                    border-radius: 50%;
-                    margin-right: $small_margin;
-                }
-
-                .title {
-                    font-style: normal;
-                    color: $minor_color;
-                    font-weight: $bold_text;
-                    text-transform: uppercase;
-                    font-size: $small_font_size;
-                    font-family: $regular_family;
-                    line-height: $small_line_height;
-                    letter-spacing: $regular_letter_spacing;
-                }
-            }
-
-            .task {
-                cursor: pointer;
-                border-radius: 8px;
-                padding: $universal_backdown $regular_margin;
-                background: $color_white;
-                box-shadow: 0px 4px 6px rgba(54, 78, 126, 0.101545);
-
-                &:not(:last-child) {
-                    margin-bottom: 20px;
-                }
-
-                &__title {
-                    font-style: normal;
-                    color: $primary_black;
-                    font-weight: $bold_text;
-                    margin-bottom: $tiny_margin;
-                    font-family: $regular_family;
-                    font-size: $regular_font_size;
-                    line-height: $regular_line_height;
-                }
-
-                .subtasks {
-                    p {
-                        font-style: normal;
-                        color: $minor_color;
-                        font-weight: $bold_text;
-                        font-size: $small_font_size;
-                        font-family: $regular_family;
-                        line-height: $small_line_height;
-                    }
-                }
-            }
-        }
+        padding-top: $universal_backdown;
 
         .add-column {
             width: 280px;
---
+++ client/src/components/Column.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="column">
+        <div class="column__header">
+            <div class="color" :style="{ background: column.color }"></div>
+            <h4 class="title">
+                {{ column.title }} ({{ column.tasks?.length }})
+            </h4>
+        </div>
+        <Task v-for="task in column.tasks" :task="task" />
+    </div>
+</template>
+
+<script setup>
+import Task from './Task.vue';
+
+const props = defineProps({
+    column: Array,
+});
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/main.scss';
+
+.column {
+    width: 280px;
+    margin-left: $universal_backdown;
+    margin-right: $universal_backdown;
+
+    .column__header {
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        margin-bottom: $universal_backdown;
+
+        .color {
+            width: 15px;
+            height: 15px;
+            border-radius: 50%;
+            margin-right: $small_margin;
+        }
+
+        .title {
+            font-style: normal;
+            color: $minor_color;
+            font-weight: $bold_text;
+            text-transform: uppercase;
+            font-size: $small_font_size;
+            font-family: $regular_family;
+            line-height: $small_line_height;
+            letter-spacing: $regular_letter_spacing;
+        }
+    }
+}
+</style>
