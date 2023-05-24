8397f47ce3b7bea4a04f4723a61fcbd83b79f07c
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2023-05-24 18:13:59 +0300

    create mutation which toggle subtasks

--- client/src/store/mutations.js
+++ client/src/store/mutations.js
@@ -10,4 +10,18 @@ export default {
     setActiveBoard(state, id) {
         state.activeBoard = state.localBoardsMap[id];
     },
+
+    toggleSubtask(state, payload) {
+        state.activeBoard.columns.map((column) => {
+            column.tasks.map((task) => {
+                if (task.id === payload.taskId) {
+                    task.subtasks.map((subtask) => {
+                        if (subtask.id === payload.subtaskId) {
+                            subtask.checked = !subtask.checked;
+                        }
+                    });
+                }
+            });
+        });
+    },
 };
