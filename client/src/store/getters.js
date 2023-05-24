17d559c6c61ed78de6544a90fa6b858f6ec04c02
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2023-05-24 18:11:39 +0300

    create getter which gets all board columns

--- client/src/store/getters.js
+++ client/src/store/getters.js
@@ -2,4 +2,10 @@ export default {
     getBoardColumnLength(state) {
         return !!state.activeBoard?.columns.length;
     },
+
+    getBoardColumns(state) {
+        return state.activeBoard.columns.map((item) => {
+            return item;
+        });
+    },
 };
