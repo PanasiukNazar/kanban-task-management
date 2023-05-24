294959cfc756167c7e7c6f1c4c0988f11e856c03
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2023-05-24 18:06:19 +0300

    connect vue-final-modal library

--- client/src/main.js
+++ client/src/main.js
@@ -4,5 +4,9 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import './styles/style.css';
+import 'vue-final-modal/style.css';
+import { createVfm } from 'vue-final-modal';
 
-createApp(App).use(store).use(router).mount('#app');
+const vfm = createVfm();
+
+createApp(App).use(store).use(router).use(vfm).mount('#app');
